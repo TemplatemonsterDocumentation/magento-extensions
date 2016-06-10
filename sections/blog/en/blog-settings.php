0373721816da654860e673451058d62cd2f28ffb
makedonen <makedonen1@gmail.com>
2016-06-10 16:10:10 +0300

    blog

---
+++ sections/blog/en/blog-settings.php
@@ -0,0 +1,271 @@
+<h2 class="item1">Extension settings</h2>
+<h3>General</h3>
+In this section, you’ll find all general options of the module. Here you can enable or disable the module, add keywords, description, etc. <br>
+In order to configure the module, navigate to <strong>Stores -> Settings -> Configuration</strong> section. <br>
+Choose the <strong>TEMPLATEMONSTER -> Blog</strong> in the left side menu to go to the module settings interface.
+<figure class="img-polaroid"><img src="img/magento/social-1.jpg" alt="" /></figure>
+
+<table  class="options-table">
+    <tr>
+        <td class="col-1"><strong>Enable/Disable </strong></td>
+        <td>-</td>
+        <td>enable/disable the module.</td>
+    </tr>     
+    <tr>
+        <td class="col-1"><strong>Show in TopLinks </strong></td>
+        <td>-</td>
+        <td>enable/disable link 'Blog' in header toplinks.</td>
+    </tr>
+    <tr>
+        <td class="col-1"><strong>Title </strong></td>
+        <td>-</td>
+        <td>specify the module title.</td>
+    </tr>
+    <tr>
+        <td class="col-1"><strong>Meta Keywords </strong></td>
+        <td>-</td>
+        <td>add meta keywords.</td>
+    </tr>
+    <tr>
+        <td class="col-1"><strong>Meta Description </strong></td>
+        <td>-</td>
+        <td>add meta description.</td>
+    </tr>
+    <tr>
+        <td class="col-1"><strong>Route </strong></td>
+        <td>-</td>
+        <td>specify the route to the blog, if "blog" will be like website.my/blog/.</td>
+    </tr>
+    <tr>
+        <td class="col-1"><strong>Posts per view </strong></td>
+        <td>-</td>
+        <td>indicate how many posts will be displayed per view.</td>
+    </tr>
+    <tr>
+        <td class="col-1"><strong>Post List Page Layout </strong></td>
+        <td>-</td>
+        <td>choose a proper layout type for the post list.</td>
+    </tr>
+    <tr>
+        <td class="col-1"><strong>Post Page Layout </strong></td>
+        <td>-</td>
+        <td>choose a proper layout type for the post page.</td>
+    </tr>
+</table>
+
+<h3>Post View</h3>
+In this section you can customize the view type of related posts and products.
+<figure class="img-polaroid"><img src="img/magento/social-2.jpg" alt="" /></figure>
+<h4>Related posts</h4>
+<table  class="options-table">
+    <tr>
+        <td class="col-1"><strong>Display Related Posts </strong></td>
+        <td>-</td>
+        <td>show/hide related posts.</td>
+    </tr>
+    <tr>
+        <td class="col-1"><strong>Number of Posts</strong></td>
+        <td>-</td>
+        <td>specify the number of related posts.</td>
+    </tr>
+</table>
+<h4>Related Products</h4>
+<table  class="options-table">
+    <tr>
+        <td class="col-1"><strong>Display Related Products </strong></td>
+        <td>-</td>
+        <td>show/hide related products.</td>
+    </tr>
+    <tr>
+        <td class="col-1"><strong>Number of Products </strong></td>
+        <td>-</td>
+        <td>specify the number of related products.</td>
+    </tr>             
+</table>
+
+
+<h3>Adding post</h3>
+<p>Navigate to the <strong>Content</strong> section in the left sidebar. Find <strong>Blog</strong> and click <strong>Posts</strong>. Here you will see the list of all posts available on your website. You can either delete or edit the posts by clicking the <strong>Select</strong> button in the <strong>Action</strong> column. <br>
+To create a new post, you need to click the  <strong>Add New Post</strong> button at the top. In the popup window, you will see several sections.</p>
+<figure class="img-polaroid"><img src="img/magento/social-3.jpg" alt="" /></figure>
+
+
+
+<h4>Post information</h4>
+In this section you need to specify all information regarding the new post.   
+<figure class="img-polaroid"><img src="img/magento/social-4.jpg" alt="" /></figure>
+
+<table  class="options-table">
+    <tr>
+        <td class="col-1"><strong>Post Title </strong></td>
+        <td>-</td>
+        <td>specify the post title.</td>
+    </tr>
+    <tr>
+        <td class="col-1"><strong>Author </strong></td>
+        <td>-</td>
+        <td>specify the author name.</td>
+    </tr>
+    <tr>
+        <td class="col-1"><strong>Identifier </strong></td>
+        <td>-</td>
+        <td>enter the identifier for the post.</td>
+    </tr>      
+    <tr>
+        <td class="col-1"><strong>Store View </strong></td>
+        <td>-</td>
+        <td>specify the store view.</td>
+    </tr>
+    <tr>
+        <td class="col-1"><strong>Status </strong></td>
+        <td>-</td>
+        <td>show/hide the post.</td>
+    </tr>   
+    <tr>
+        <td class="col-1"><strong>Comments </strong></td>
+        <td>-</td>
+        <td>enable/disable comments.</td>
+    </tr> 
+    <tr>
+        <td class="col-1"><strong>Post Date </strong></td>
+        <td>-</td>
+        <td>choose the post date.</td>
+    </tr>         
+</table>
+
+
+<h4>Content</h4>
+Here you can add content images and widgets.   
+<figure class="img-polaroid"><img src="img/magento/social-5.jpg" alt="" /></figure>
+<table  class="options-table">
+    <tr>
+        <td class="col-1"><strong>Image </strong></td>
+        <td>-</td>
+        <td>upload the featured image for the post.</td>
+    </tr>
+    <tr>
+        <td class="col-1"><strong>Tags </strong></td>
+        <td>-</td>
+        <td>add the required tags.</td>
+    </tr>
+    <tr>
+        <td class="col-1"><strong>Content </strong></td>
+        <td>-</td>
+        <td>add the content.</td>
+    </tr>
+    <tr>
+        <td class="col-1"><strong>Show/Hide Editor</strong></td>
+        <td>-</td>
+        <td>insert a specific widget, image and variable.</td>
+    </tr>       
+    <tr>
+        <td class="col-1"><strong>Short content </strong></td>
+        <td>-</td>
+        <td>Add short content that will be displayed on the blog page.</td>
+    </tr>
+    <tr>
+        <td class="col-1"><strong>Short content </strong></td>
+        <td>-</td>
+        <td>Add short content that will be displayed on the blog page.</td>
+    </tr>  
+    <tr>
+        <td class="col-1"><strong>Short content </strong></td>
+        <td>-</td>
+        <td>Add short content that will be displayed on the blog page.</td>
+    </tr>  
+</table>
+
+<h4>Meta Data</h4>
+<p>This section contains meta data for your post. You need to specify keywords and short description here.</p>
+<figure class="img-polaroid"><img src="img/magento/social-5.jpg" alt="" /></figure>
+<table  class="options-table">
+    <tr>
+        <td class="col-1"><strong>Keywords </strong></td>
+        <td>-</td>
+        <td>add proper keywords.</td>
+    </tr>
+    <tr>
+        <td class="col-1"><strong>Description </strong></td>
+        <td>-</td>
+        <td>add short description.</td>
+    </tr>    
+</table>
+
+<h4>Related Products</h4>
+<p>Here you can choose related products that will be displayed on the post page. Check the products you need to add and specify position of the products</p>
+<figure class="img-polaroid"><img src="img/magento/social-5.jpg" alt="" /></figure>
+
+<h4>Related Posts</h4>
+<p>This section works the same way as the <strong>Related Products</strong> section.</p>
+<figure class="img-polaroid"><img src="img/magento/social-5.jpg" alt="" /></figure>
+
+<h4>Post Comments</h4>
+<p>In this section you can see all the comments for a particular post. You can edit and display each of them. To do this, you need to click the <strong>Edit</strong> button in the right column. In the popup window you will see the following information:</p>
+<figure class="img-polaroid"><img src="img/magento/social-5.jpg" alt="" /></figure>
+<table  class="options-table">
+    <tr>
+        <td class="col-1"><strong>Author </strong></td>
+        <td>-</td>
+        <td>author name.</td>
+    </tr>
+    <tr>
+        <td class="col-1"><strong>Comment </strong></td>
+        <td>-</td>
+        <td>comment text.</td>
+    </tr>
+    <tr>
+        <td class="col-1"><strong>Status </strong></td>
+        <td>-</td>
+        <td>show or hide comments.</td>
+    </tr>
+    <tr>
+        <td class="col-1"><strong>Comment Date </strong></td>
+        <td>-</td>
+        <td>change the comment publish date.</td>
+    </tr>    
+</table>
+
+<h3>Blog: Latest Posts Widget</h3>
+<p>To add a Blog widget to the homepage, you need to navigate to <strong>Content > Widgets</strong> tab. At the bottom of the page, you’ll see the Blog Widget. Now you can start customizing it:</p>
+<figure class="img-polaroid"><img src="img/magento/social-5.jpg" alt="" /></figure>
+
+<h4>Storefront Properties</h4>
+<p>Here you can find a list of options that allow customizing the widget and making it match the corporate style of your business.</p>
+<figure class="img-polaroid"><img src="img/magento/social-5.jpg" alt="" /></figure>
+<table  class="options-table">
+    <tr>
+        <td class="col-1"><strong>Type </strong></td>
+        <td>-</td>
+        <td>choose a proper widget type. This option is available only when you add a new widget.</td>
+    </tr>
+    <tr>
+        <td class="col-1"><strong>Design Package/Theme </strong></td>
+        <td>-</td>
+        <td>choose a theme to which you want to add a widget. This option is available only when you add a new widget.</td>
+    </tr>
+    <tr>
+        <td class="col-1"><strong>Widget Title </strong></td>
+        <td>-</td>
+        <td>specify the widget title.</td>
+    </tr>
+    <tr>
+        <td class="col-1"><strong>Assign to Store Views </strong></td>
+        <td>-</td>
+        <td>assign the widget to a particular store view.</td>
+    </tr>
+    <tr>
+        <td class="col-1"><strong>Sort Order </strong></td>
+        <td>-</td>
+        <td>sort order of widget instances.</td>
+    </tr>
+</table>
+
+<h4>Widget Options</h4>
+<figure class="img-polaroid"><img src="img/magento/social-5.jpg" alt="" /></figure>
+<table  class="options-table">
+    <tr>
+        <td class="col-1"><strong>Post amount </strong></td>
+        <td>-</td>
+        <td>number of posts displayed on the page.</td>
+    </tr>
+</table>
