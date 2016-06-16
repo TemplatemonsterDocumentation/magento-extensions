479fa7aa80e04dca35a40dc051e24a876a51bb16
makedonen <makedonen1@gmail.com>
2016-06-16 17:42:12 +0300

    new

--- sections/blog/en/blog-settings.php
+++ sections/blog/en/blog-settings.php
@@ -3,7 +3,7 @@
 In this section, you’ll find all general options of the module. Here you can enable or disable the module, add keywords, description, etc. <br>
 In order to configure the module, navigate to <strong>Stores -> Settings -> Configuration</strong> section. <br>
 Choose the <strong>TEMPLATEMONSTER -> Blog</strong> in the left side menu to go to the module settings interface.
-<figure class="img-polaroid"><img src="img/magento/social-1.jpg" alt="" /></figure>
+<figure class="img-polaroid"><img src="img/magento/blog-1.jpg" alt="" /></figure>
 
 <table  class="options-table">
     <tr>
@@ -55,7 +55,7 @@ Choose the <strong>TEMPLATEMONSTER -> Blog</strong> in the left side menu to go 
 
 <h3>Post View</h3>
 In this section you can customize the view type of related posts and products.
-<figure class="img-polaroid"><img src="img/magento/social-2.jpg" alt="" /></figure>
+<figure class="img-polaroid"><img src="img/magento/blog-2.jpg" alt="" /></figure>
 <h4>Related posts</h4>
 <table  class="options-table">
     <tr>
@@ -87,13 +87,13 @@ In this section you can customize the view type of related posts and products.
 <h3>Adding post</h3>
 <p>Navigate to the <strong>Content</strong> section in the left sidebar. Find <strong>Blog</strong> and click <strong>Posts</strong>. Here you will see the list of all posts available on your website. You can either delete or edit the posts by clicking the <strong>Select</strong> button in the <strong>Action</strong> column. <br>
 To create a new post, you need to click the  <strong>Add New Post</strong> button at the top. In the popup window, you will see several sections.</p>
-<figure class="img-polaroid"><img src="img/magento/social-3.jpg" alt="" /></figure>
+<figure class="img-polaroid"><img src="img/magento/blog-3.jpg" alt="" /></figure>
 
 
 
 <h4>Post information</h4>
 In this section you need to specify all information regarding the new post.   
-<figure class="img-polaroid"><img src="img/magento/social-4.jpg" alt="" /></figure>
+<figure class="img-polaroid"><img src="img/magento/blog-4.jpg" alt="" /></figure>
 
 <table  class="options-table">
     <tr>
@@ -136,7 +136,7 @@ In this section you need to specify all information regarding the new post.
 
 <h4>Content</h4>
 Here you can add content images and widgets.   
-<figure class="img-polaroid"><img src="img/magento/social-5.jpg" alt="" /></figure>
+<figure class="img-polaroid"><img src="img/magento/blog-5.jpg" alt="" /></figure>
 <table  class="options-table">
     <tr>
         <td class="col-1"><strong>Image </strong></td>
@@ -177,7 +177,7 @@ Here you can add content images and widgets.
 
 <h4>Meta Data</h4>
 <p>This section contains meta data for your post. You need to specify keywords and short description here.</p>
-<figure class="img-polaroid"><img src="img/magento/social-5.jpg" alt="" /></figure>
+<figure class="img-polaroid"><img src="img/magento/blog-6.jpg" alt="" /></figure>
 <table  class="options-table">
     <tr>
         <td class="col-1"><strong>Keywords </strong></td>
@@ -193,15 +193,17 @@ Here you can add content images and widgets.
 
 <h4>Related Products</h4>
 <p>Here you can choose related products that will be displayed on the post page. Check the products you need to add and specify position of the products</p>
-<figure class="img-polaroid"><img src="img/magento/social-5.jpg" alt="" /></figure>
+<figure class="img-polaroid"><img src="img/magento/blog-7.jpg" alt="" /></figure>
 
 <h4>Related Posts</h4>
 <p>This section works the same way as the <strong>Related Products</strong> section.</p>
-<figure class="img-polaroid"><img src="img/magento/social-5.jpg" alt="" /></figure>
+<figure class="img-polaroid"><img src="img/magento/blog-8.jpg" alt="" /></figure>
 
 <h4>Post Comments</h4>
-<p>In this section you can see all the comments for a particular post. You can edit and display each of them. To do this, you need to click the <strong>Edit</strong> button in the right column. In the popup window you will see the following information:</p>
-<figure class="img-polaroid"><img src="img/magento/social-5.jpg" alt="" /></figure>
+<p>In this section you can see all the comments for a particular post. You can edit and display each of them. To do this, you need to click the <strong>Edit</strong> button in the right column. 
+<figure class="img-polaroid"><img src="img/magento/blog-9.jpg" alt="" /></figure>
+In the popup window you will see the following information:</p>
+<figure class="img-polaroid"><img src="img/magento/blog-10.jpg" alt="" /></figure>
 <table  class="options-table">
     <tr>
         <td class="col-1"><strong>Author </strong></td>
@@ -227,11 +229,11 @@ Here you can add content images and widgets.
 
 <h3>Blog: Latest Posts Widget</h3>
 <p>To add a Blog widget to the homepage, you need to navigate to <strong>Content > Widgets</strong> tab. At the bottom of the page, you’ll see the Blog Widget. Now you can start customizing it:</p>
-<figure class="img-polaroid"><img src="img/magento/social-5.jpg" alt="" /></figure>
+<figure class="img-polaroid"><img src="img/magento/blog-11.jpg" alt="" /></figure>
 
 <h4>Storefront Properties</h4>
 <p>Here you can find a list of options that allow customizing the widget and making it match the corporate style of your business.</p>
-<figure class="img-polaroid"><img src="img/magento/social-5.jpg" alt="" /></figure>
+<figure class="img-polaroid"><img src="img/magento/blog-12.jpg" alt="" /></figure>
 <table  class="options-table">
     <tr>
         <td class="col-1"><strong>Type </strong></td>
@@ -261,7 +263,7 @@ Here you can add content images and widgets.
 </table>
 
 <h4>Widget Options</h4>
-<figure class="img-polaroid"><img src="img/magento/social-5.jpg" alt="" /></figure>
+<figure class="img-polaroid"><img src="img/magento/blog-13.jpg" alt="" /></figure>
 <table  class="options-table">
     <tr>
         <td class="col-1"><strong>Post amount </strong></td>
