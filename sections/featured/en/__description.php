8e2030be28fccb51506c0b5ef81fe3cbc39f54af
makedonen <makedonen1@gmail.com>
2016-08-23 09:01:52 +0300

    new

--- sections/featured/en/__description.php
+++ sections/featured/en/__description.php
@@ -1,4 +1,5 @@
 <h2 class="item1">Introduction</h2>
+<p>This widget will become in hand if you need to add a particular set of products to your home page. It offers several ready-made filters that allow webmasters to categorize the products into 6 groups. New products can be added to each of these groups as well.</p>
 The products will refer to:
 <ul class="marked-list">
   <li><strong>Bestsellers</strong> - allows displaying products with the highest amount of sales in last 6 month.</li>
