b2ed4420a48f2893b9e3d2fd04aab59113afdc5e
makedonen <makedonen1@gmail.com>
2016-09-28 12:48:20 +0300

    new

---
+++ sections/site-maintenance/en/__description.php
@@ -0,0 +1,15 @@
+<h2 class="item1">Introduction</h2>
+<p>
+    The <strong>Site Maintenance</strong> module for Magento 2 allows you to display the “Site under maintenance” page and let your customers know that now there is some technical work going on and that the website will soon become available. <br>
+		With the help of this plugin, you can change the appearance of this page. You can whitelist IPs, which allows users gain access to the frontend. You can specify the end time of the service work and display a timer to let your clients know the timing of this work. The module allows you display a subscription form for this page, and in the end of the work you can send e-mail notifications to your clients.
+</p>
+<strong>Features:</strong>
+<ul class="marked-list">
+	<li>Fully customizable (change colors, texts and backgrounds);</li>
+	<li>Subscription form (export emails to .csv file);</li>
+	<li>Countdown timer (remaining time);</li>
+	<li>Magento multisite;</li>
+	<li>Responsive design;</li>
+	<li>SEO options.</li>
+</ul>
+<figure class="img-polaroid"><img src="img/magento/maintenance-1.png" alt="" /></figure>
