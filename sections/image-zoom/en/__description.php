bf19a1b3963d37ab20d27ced0eaea6d1c570a4fa
chris <chris@templatemonster.me>
2016-12-06 17:49:39 +0200

    Add image zoom documentation files

---
+++ sections/image-zoom/en/__description.php
@@ -0,0 +1,6 @@
+<h2 class="item1">Introduction</h2>
+<p><strong>Image Zoom</strong> module is a Magento 2 Extension that implements jQuery ElevateZoom Plus Plugin into
+    Magento online store. It replaces default product image zoom (photorama) script functionality with more flexible
+    and feature-rich solution.</p>
+
+<figure class="img-polaroid"><img src="img/magento/image-zoom_1.jpg" alt="" /></figure>
--- sections/theme-updater/pl/__description.php
+++
@@ -1,6 +0,0 @@
-<h2 class="item1">Introduction</h2>
-<p><strong>Theme Updater</strong> is a Magento 2 extension that allows you to receive updates for installed themes
-    purchased at Templatemonster.com. Module periodically searches for updates and notifies you if any
-    available. You can also check for updates manually at the module configuration page.</p>
-
-<figure class="img-polaroid"><img src="img/magento/theme-updater_2.jpg" alt="" /></figure>
